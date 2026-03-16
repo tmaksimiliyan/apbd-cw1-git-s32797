7a3e0084b58577407564af63499ea6bf64ea79e7
Maksimiliyan Taraniuk <s32797@pjwstk.edu.pl>
2026-03-16 22:16:03 +0100

    Add max calculation for integer arrays

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -21,4 +21,17 @@ public class StatisticsHelper
         }
         return (double) sum / values.Length;
     }
+
+    public static int CalculateMax(int[] values)
+    {
+        int max = values[0];
+        for (int i = 0; i < values.Length; i++)
+        {
+            if (values[i] > max)
+            {
+                max = values[i];
+            }
+        }
+        return max;
+    }
 }
