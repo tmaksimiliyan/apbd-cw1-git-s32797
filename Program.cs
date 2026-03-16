206ac665cd53f42bf664d4fb77ed2706a3a70ef8
Maksimiliyan Taraniuk <s32797@pjwstk.edu.pl>
2026-03-16 21:51:09 +0100

    Add user input handling

--- Program.cs
+++ Program.cs
@@ -1,1 +1,15 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using apbd_cw1_git_s32797;
+
+Console.WriteLine("Podaj liczby oddzielone spacjami:");
+string input = Console.ReadLine() ?? "";
+
+string[] parts = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+double[] numbers = new  double[parts.Length];
+
+for (int i = 0; i < parts.Length; i++)
+{
+    numbers[i] = double.Parse(parts[i]);
+}
+
+double average = StatisticsHelper.CalculateAverage(numbers);
+Console.WriteLine($"Średnia: {average}");
